f4af3af94d571a4d631ce67b283bbd8b7535342c
jashia <jashia.jm@gmail.com>
2025-08-18 01:25:17 -0400

    multiple req

--- token_bucket/test_cases.py
+++ token_bucket/test_cases.py
@@ -3,5 +3,11 @@ from token_bucket import my_bucket
 
 def test_intial():
     bucket=my_bucket(capacity=5, drop_rate=2)
-    assert bucket.allow_req()==True
+    assert bucket.allow_req(req_cap=2)==True
+
+def test_intial_2():
+    bucket=my_bucket(capacity=5, drop_rate=2)
+    assert bucket.allow_req(req_cap=3)==True
+    assert bucket.allow_req(req_cap=2)==True
+    assert bucket.allow_req(req_cap=3)==True
 
--- token_bucket/token_bucket.py
+++ token_bucket/token_bucket.py
@@ -2,10 +2,10 @@ class my_bucket:
     def __init__(self,capacity,drop_rate):
         self.capacity=capacity
         self.drop_rate=drop_rate
+        
 
-    def allow_req(self):
+    def allow_req(self,req_cap):
         if self.capacity>=1:
-            self.capacity-=1
+            self.capacity-=req_cap
             return True
         return False
-        
